43cae46aaa23e01ff92b794b4af81d8f4ceab51e
modibo <modibocoulibaly464@gmail.com>
2026-04-07 00:27:55 +0200

    backend réel supergo

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  const port = process.env.PORT || 3001;
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Serveur prêt sur le port ${port}`);
+}
+
+bootstrap();
