ba349a2750a095d7821b40b144d5e43ff093a7f8
modibo <modibocoulibaly464@gmail.com>
2026-04-07 18:21:25 +0200

    Fix: rename source to src for NestJS compatibility

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   app.enableCors();
 
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
 
   console.log(`Serveur prêt sur le port ${port}`);
